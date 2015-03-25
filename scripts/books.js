6f386d31a1e376faabce06d17cd678cffd5041b0
jayspringate <jay.springate@gmail.com>
2015-03-25 14:50:38 -0700

    adding library model including separate modules for books, shelves, and the library

---
+++ scripts/books.js
@@ -0,0 +1,9 @@
+function Book (author, title) {
+	this.author = author;
+	this.title = title;
+};
+
+var redBook = new Book ('Sally', 'redBook');
+var greenBook = new Book ('Jane', 'greenBook');
+var sadBook = new Book ('Ed', 'sadBook');
+var happyBook = new Book ('Lisa', 'happyBook');
